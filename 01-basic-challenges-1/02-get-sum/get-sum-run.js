6d5c89279d4f0ebc2a2540c397c8d10d7781e2cf
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2024-08-10 13:12:03 +0530

    DSA JS

---
+++ 01-basic-challenges-1/02-get-sum/get-sum-run.js
@@ -0,0 +1,5 @@
+const getSum = require('./get-sum');
+
+const result = getSum(2, 2);
+
+console.log(result);
---
+++ 01-basic-challenges-1/03-calculator/calculator-run.js
@@ -0,0 +1,5 @@
+const calculator = require('./calculator');
+
+const result = calculator(2, 3, '*');
+
+console.log(result);
